3abe707f4fa706eb329f8d75ee8137182b086897
monseg73 <monsegcar@gmail.com>
2022-01-20 22:42:36 +0100

    Valido cambio metodo main

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -5,6 +5,7 @@ import java.io.RandomAccessFile;
 public class AleatorioEmple {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println ("Estoy en el métono main\n");
 		crear();
 		leer();
 	}
