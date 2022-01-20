a18fd8674de07b0bf2bb60cb46d111a7d6e9d5ae
monseg73 <monsegcar@gmail.com>
2022-01-20 22:43:29 +0100

    valido método leer

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -11,6 +11,7 @@ public class AleatorioEmple {
 	}
 
 	public static void leer() throws IOException {
+		System.out.println("Estoy en el método leer\n");
 		File fichero = new File("AleatorioEmple.dat");
 		RandomAccessFile file = new RandomAccessFile(fichero, "r");
 		char cad[] = new char[10], aux;
